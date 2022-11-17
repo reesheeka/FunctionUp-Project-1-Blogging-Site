84f51a90054a1d9d917cf7605d1d87ed936c7037
birendra8757 <computersinghaniya@gmail.com>
2022-11-17 18:18:49 +0530

    group14

--- routes/route.js
+++ routes/route.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const authorController = require("../Controllers/authorController");
 const blogController = require("../Controllers/blogController");
 const MW = require("../Middlewares/auth");
@@ -39,6 +40,18 @@ router.all("/*", function (req, res) {
 
 
 
+=======
+const AuthorController= require("../controllers/authorController")
+const BlogController= require("../controllers/blogController")
+
+const router = express.Router();
+
+router.post('/createAuthor', AuthorController.createAuthor)
+
+router.post('/createBlog', BlogController.createBlog)
+
+router.get('/getBlog', BlogController.getBlog)
+>>>>>>> 71979613dbfcf9c9eedc82f2718a431fe0304ff5
 
 
 
